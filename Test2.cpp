977be221db78cd080496b2f5a5246686e54d628e
a452402 <151842583+a452402@users.noreply.github.com>
2024-03-15 13:06:10 +0530

    Add files via upload

---
+++ Test2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+  char name[10];
+ 
+  cout << "Enter your name" ;
+  cin  >> name;
+ 
+  cout  << "Hello, " <<   name << endl;
+ 
+  return 0;
+}
